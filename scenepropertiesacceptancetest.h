64ce8504d01caed0f5f9efe2967cd044a4bbf69b
Hendrik Vennekate <Hendrik.Vennekate@posteo.de>
2016-12-30 23:12:09 +0100

    Add acceptance test for scene property not getting changed when Esc is pressed or drag entered.

---
+++ scenepropertiesacceptancetest.h
@@ -0,0 +1,62 @@
+/***************************************************************************
+ *   Copyright (C) 2016 by Hendrik Vennekate                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <QCoreApplication>
+#include <QGraphicsSceneContextMenuEvent>
+#include <QKeyEvent>
+#include <QMimeData>
+#include <molscene.h>
+#include <cxxtest/TestSuite.h>
+
+using namespace Molsketch;
+
+class ScenePropertiesAcceptanceTest : public CxxTest::TestSuite {
+  MolScene *scene;
+public:
+  void setUp() {
+    scene = new MolScene;
+  }
+
+  void tearDown() {
+    delete scene;
+  }
+
+  void testPropertyUnchangedOnEscapeKeyPress() {
+    // TODO iterate over all properties
+    scene->setChargeVisible(true);
+
+    QKeyEvent *event = new QKeyEvent(QKeyEvent::KeyPress, Qt::Key_Escape, Qt::NoModifier);
+    QCoreApplication::sendEvent(scene, event);
+    delete event;
+    TS_ASSERT(scene->chargeVisible());
+  }
+
+  void testPropertyUnchangedOnDragEnterEvent() {
+    scene->setChargeVisible(true);
+
+    QGraphicsSceneDragDropEvent *event = new QGraphicsSceneDragDropEvent(QEvent::GraphicsSceneDragEnter);
+    QMimeData *data = new QMimeData;
+    data->setData(scene->mimeType(), QByteArray());
+    event->setMimeData(data);
+    event->setProposedAction(Qt::CopyAction);
+    QCoreApplication::sendEvent(scene, event);
+    delete event;
+    TS_ASSERT(scene->chargeVisible());
+  }
+};
