77d5d9c86e840ac5d58aed7fec636d0340185641
Hendrik Vennekate <Hendrik.Vennekate@posteo.de>
2017-01-03 13:22:47 +0100

    Add test for inserting molecule from library (drag-and-drop)

---
+++ moleculelibraryacceptancetest.h
@@ -0,0 +1,109 @@
+/***************************************************************************
+ *   Copyright (C) 2016 by Hendrik Vennekate                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <cxxtest/TestSuite.h>
+#include "utilities.h"
+#include <molscene.h>
+#include <molview.h>
+#include <QByteArray>
+#include <QXmlStreamWriter>
+#include <molecule.h>
+#include <QMimeData>
+#include <QGraphicsSceneDragDropEvent>
+#include <QCoreApplication>
+#include <QDebug>
+#include <algorithm>
+#include <QGraphicsItem>
+
+using namespace Molsketch;
+
+class MoleculeLibraryAcceptanceTest : public CxxTest::TestSuite {
+  QMimeData* prepareMimeData() {
+    QMimeData *mimeData = new QMimeData;
+    mimeData->setData(MolScene::mimeType(), serializeMolecule(sampleMolecule()));
+    return mimeData;
+  }
+
+  QByteArray serializeMolecule(const Molecule& molecule) {
+    QByteArray data;
+    QXmlStreamWriter xmlWriter(&data);
+    xmlWriter.writeStartDocument();
+    xmlWriter << molecule;
+    xmlWriter.writeEndDocument();
+    return data;
+  }
+
+  Molecule sampleMolecule() {
+    Atom *atomA = new Atom(QPointF(5,-5), "C");
+    Atom *atomB = new Atom(QPointF(45,35), "C");
+    return Molecule(QSet<Atom*>() << atomA << atomB,
+                    QSet<Bond*>() << new Bond(atomA, atomB));
+  }
+
+  void prepareAndSendEvent(QEvent::Type type) {
+    QGraphicsSceneDragDropEvent *event = new QGraphicsSceneDragDropEvent(type);
+    event->setMimeData(prepareMimeData());
+    event->setProposedAction(Qt::CopyAction);
+    event->setScenePos(QPointF(10,5));
+    TS_ASSERT(QCoreApplication::sendEvent(scene, event));
+  }
+
+  void assertMoleculeAndAtomCoordinates(MolScene* scene) {
+    assertMoleculePosition(scene);
+    assertAtomPositions(scene);
+  }
+
+  void assertMoleculePosition(MolScene* scene)
+  {
+    Molecule* moleculeItem = 0;
+    for (QGraphicsItem* item : scene->items())
+      if(dynamic_cast<Molecule*>(item))
+        moleculeItem = dynamic_cast<Molecule*>(item);
+    QS_ASSERT_ON_POINTER(Molecule, moleculeItem, pos(), QPointF(0,0));
+  }
+
+  void assertAtomPositions(MolScene* scene)
+  {
+    QS_ASSERT_ON_POINTER(Atom, scene->itemAt(-10, -15, QTransform()), coordinates(), QPolygonF() << QPointF(-10, -15));
+    QS_ASSERT_ON_POINTER(Atom, scene->itemAt(-10, -15, QTransform()), pos(), QPointF(-10, -15));
+    QS_ASSERT_ON_POINTER(Atom, scene->itemAt( 30,  25, QTransform()), coordinates(), QPolygonF() << QPointF(30, 25));
+    QS_ASSERT_ON_POINTER(Atom, scene->itemAt( 30,  25, QTransform()), pos(), QPointF(30, 25));
+  }
+
+  MolScene *scene;
+public:
+  void setUp() {
+    scene = new MolScene;
+  }
+
+  void tearDown() {
+    delete scene;
+  }
+
+  void testBondsCanBeAddedAfterInsertingMolecule() {
+    prepareAndSendEvent(QEvent::GraphicsSceneDragEnter);
+    assertMoleculeAndAtomCoordinates(scene);
+
+    prepareAndSendEvent(QEvent::GraphicsSceneDrop);
+    assertMoleculeAndAtomCoordinates(scene);
+  }
+
+
+  // TODO check for multiple molecules
+};
--- utilities.h
+++ utilities.h
@@ -10,6 +10,15 @@ class QTableView;
 
 #define QS_ASSERT_EQUALS(VAL1,VAL2) {QString __comparison; QDebug __out(&__comparison); __out << VAL1; __comparison += "!= "; __out << VAL2; TSM_ASSERT_EQUALS(__comparison.toStdString().data(), VAL1, VAL2)}
 
+#define QS_ASSERT_ON_POINTER(CAST_TYPE, POINTER, METHOD, VALUE) {\
+  TSM_ASSERT(#POINTER " is null", POINTER);\
+  if (POINTER) {\
+    TSM_ASSERT(#POINTER " is not of type " #CAST_TYPE, dynamic_cast<CAST_TYPE*>(POINTER));\
+    CAST_TYPE *p = dynamic_cast<CAST_TYPE*>(POINTER);\
+    if (p) QS_ASSERT_EQUALS(p->METHOD, VALUE)\
+  }\
+  }
+
 void mouseMoveEvent(QWidget *widget, Qt::MouseButton button, Qt::KeyboardModifiers stateKey, QPoint pos, int delay=-1);
 
 void enterDataIntoCell(QTableView *table, const QString& data, int row, int column);
