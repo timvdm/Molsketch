b8051374d0d70d602052cc18d744c559efcce335
Hendrik Vennekate <Hendrik.Vennekate@posteo.de>
2017-08-28 17:36:06 +0200

    add offset to bounding box linker

--- boundingboxlinkerunittest.h
+++ boundingboxlinkerunittest.h
@@ -36,12 +36,120 @@ class BoundingBoxLinkerUnitTest : public CxxTest::TestSuite {
     return linker.getShift(reference, subject);
   }
 
+  QPointF getShiftWithOffset(const QRectF& reference, const QRectF& subject,
+                             Anchor referenceAnchor, Anchor subjectAnchor,
+                             const QPointF& offset) {
+    BoundingBoxLinker linker(referenceAnchor, subjectAnchor, offset);
+    return linker.getShift(reference, subject);
+  }
+
   void assertShift(const QRectF& reference, const QRectF& subject,
                    Anchor referenceAnchor, Anchor subjectAnchor,
                    const QPointF& expected) {
     QPointF actual = getShift(reference, subject, referenceAnchor, subjectAnchor);
     QString message = "Mismatch for reference anchor " + QString::number(referenceAnchor) + " and subject anchor " + QString::number(subjectAnchor) + ". Expected: " + toString(expected) + " Actual: " + toString(actual);
     QSM_ASSERT_EQUALS(message, actual, expected);
+  }
+
+  void assertShiftWithOffset(const QRectF& reference, const QRectF& subject,
+                             Anchor referenceAnchor, Anchor subjectAnchor,
+                             const QPointF& offset, const QPointF& expected) {
+    QPointF actual = getShiftWithOffset(reference, subject, referenceAnchor, subjectAnchor, offset);
+    QString message = "Mismatch for reference anchor " + QString::number(referenceAnchor) + " and subject anchor " + QString::number(subjectAnchor) + " with offset " + toString(offset) + ". Expected: " + toString(expected) + " Actual: " + toString(actual);
+    QSM_ASSERT_EQUALS(message, actual, expected);
+  }
+
+  QVector<std::tuple<Anchor, Anchor, QPointF>> testCases() {
+    return QVector<std::tuple<Anchor, Anchor, QPointF>>()
+        << std::make_tuple(Center, Center, QPointF(1.625, 20.875))
+        << std::make_tuple(Center, Top, QPointF(1.625, 22.875))
+        << std::make_tuple(Center, Bottom, QPointF(1.625, 18.875))
+        << std::make_tuple(Center, Left, QPointF(3.375, 20.875))
+        << std::make_tuple(Center, Right, QPointF(-.125, 20.875))
+        << std::make_tuple(Center, TopLeft, QPointF(3.375, 22.875))
+        << std::make_tuple(Center, BottomLeft, QPointF(3.375, 18.875))
+        << std::make_tuple(Center, TopRight, QPointF(-.125, 22.875))
+        << std::make_tuple(Center, BottomRight, QPointF(-.125, 18.875))
+
+        << std::make_tuple(Top, Center, QPointF(1.625, 14.375))
+        << std::make_tuple(Top, Top, QPointF(1.625, 16.375))
+        << std::make_tuple(Top, Bottom, QPointF(1.625, 12.375))
+        << std::make_tuple(Top, Left, QPointF(3.375, 14.375))
+        << std::make_tuple(Top, Right, QPointF(-.125, 14.375))
+        << std::make_tuple(Top, TopLeft, QPointF(3.375, 16.375))
+        << std::make_tuple(Top, BottomLeft, QPointF(3.375, 12.375))
+        << std::make_tuple(Top, TopRight, QPointF(-.125, 16.375))
+        << std::make_tuple(Top, BottomRight, QPointF(-.125, 12.375))
+
+        << std::make_tuple(Bottom, Center, QPointF(1.625, 27.375))
+        << std::make_tuple(Bottom, Top, QPointF(1.625, 29.375))
+        << std::make_tuple(Bottom, Bottom, QPointF(1.625, 25.375))
+        << std::make_tuple(Bottom, Left, QPointF(3.375, 27.375))
+        << std::make_tuple(Bottom, Right, QPointF(-.125, 27.375))
+        << std::make_tuple(Bottom, TopLeft, QPointF(3.375, 29.375))
+        << std::make_tuple(Bottom, BottomLeft, QPointF(3.375, 25.375))
+        << std::make_tuple(Bottom, TopRight, QPointF(-.125, 29.375))
+        << std::make_tuple(Bottom, BottomRight, QPointF(-.125, 25.375))
+
+        << std::make_tuple(Left, Center, QPointF(-8.625, 20.875))
+        << std::make_tuple(Left, Top, QPointF(-8.625, 22.875))
+        << std::make_tuple(Left, Bottom, QPointF(-8.625, 18.875))
+        << std::make_tuple(Left, Left, QPointF(-6.875, 20.875))
+        << std::make_tuple(Left, Right, QPointF(-10.375, 20.875))
+        << std::make_tuple(Left, TopLeft, QPointF(-6.875, 22.875))
+        << std::make_tuple(Left, BottomLeft, QPointF(-6.875, 18.875))
+        << std::make_tuple(Left, TopRight, QPointF(-10.375, 22.875))
+        << std::make_tuple(Left, BottomRight, QPointF(-10.375, 18.875))
+
+        << std::make_tuple(Right, Center, QPointF(11.875, 20.875))
+        << std::make_tuple(Right, Top, QPointF(11.875, 22.875))
+        << std::make_tuple(Right, Bottom, QPointF(11.875, 18.875))
+        << std::make_tuple(Right, Left, QPointF(13.625, 20.875))
+        << std::make_tuple(Right, Right, QPointF(10.125, 20.875))
+        << std::make_tuple(Right, TopLeft, QPointF(13.625, 22.875))
+        << std::make_tuple(Right, BottomLeft, QPointF(13.625, 18.875))
+        << std::make_tuple(Right, TopRight, QPointF(10.125, 22.875))
+        << std::make_tuple(Right, BottomRight, QPointF(10.125, 18.875))
+
+        << std::make_tuple(TopLeft, Center, QPointF(-8.625, 14.375))
+        << std::make_tuple(TopLeft, Top, QPointF(-8.625, 16.375))
+        << std::make_tuple(TopLeft, Bottom, QPointF(-8.625, 12.375))
+        << std::make_tuple(TopLeft, Left, QPointF(-6.875, 14.375))
+        << std::make_tuple(TopLeft, Right, QPointF(-10.375, 14.375))
+        << std::make_tuple(TopLeft, TopLeft, QPointF(-6.875, 16.375))
+        << std::make_tuple(TopLeft, BottomLeft, QPointF(-6.875, 12.375))
+        << std::make_tuple(TopLeft, TopRight, QPointF(-10.375, 16.375))
+        << std::make_tuple(TopLeft, BottomRight, QPointF(-10.375, 12.375))
+
+        << std::make_tuple(BottomLeft, Center, QPointF(-8.625, 27.375))
+        << std::make_tuple(BottomLeft, Top, QPointF(-8.625, 29.375))
+        << std::make_tuple(BottomLeft, Bottom, QPointF(-8.625, 25.375))
+        << std::make_tuple(BottomLeft, Left, QPointF(-6.875, 27.375))
+        << std::make_tuple(BottomLeft, Right, QPointF(-10.375, 27.375))
+        << std::make_tuple(BottomLeft, TopLeft, QPointF(-6.875, 29.375))
+        << std::make_tuple(BottomLeft, BottomLeft, QPointF(-6.875, 25.375))
+        << std::make_tuple(BottomLeft, TopRight, QPointF(-10.375, 29.375))
+        << std::make_tuple(BottomLeft, BottomRight, QPointF(-10.375, 25.375))
+
+        << std::make_tuple(TopRight, Center, QPointF(11.875, 14.375))
+        << std::make_tuple(TopRight, Top, QPointF(11.875, 16.375))
+        << std::make_tuple(TopRight, Bottom, QPointF(11.875, 12.375))
+        << std::make_tuple(TopRight, Left, QPointF(13.625, 14.375))
+        << std::make_tuple(TopRight, Right, QPointF(10.125, 14.375))
+        << std::make_tuple(TopRight, TopLeft, QPointF(13.625, 16.375))
+        << std::make_tuple(TopRight, BottomLeft, QPointF(13.625, 12.375))
+        << std::make_tuple(TopRight, TopRight, QPointF(10.125, 16.375))
+        << std::make_tuple(TopRight, BottomRight, QPointF(10.125, 12.375))
+
+        << std::make_tuple(BottomRight, Center, QPointF(11.875, 27.375))
+        << std::make_tuple(BottomRight, Top, QPointF(11.875, 29.375))
+        << std::make_tuple(BottomRight, Bottom, QPointF(11.875, 25.375))
+        << std::make_tuple(BottomRight, Left, QPointF(13.625, 27.375))
+        << std::make_tuple(BottomRight, Right, QPointF(10.125, 27.375))
+        << std::make_tuple(BottomRight, TopLeft, QPointF(13.625, 29.375))
+        << std::make_tuple(BottomRight, BottomLeft, QPointF(13.625, 25.375))
+        << std::make_tuple(BottomRight, TopRight, QPointF(10.125, 29.375))
+        << std::make_tuple(BottomRight, BottomRight, QPointF(10.125, 25.375));
   }
 
 public:
@@ -54,96 +162,16 @@ public:
   void testMovingToAnchor() {
     QRectF referenceBoundingBox(3.5, 5.125, 20.5, 13),
         ownBoundingBox(10.375, -11.25, 3.5, 4);
-
-    assertShift(referenceBoundingBox, ownBoundingBox, Center, Center, QPointF(1.625, 20.875));
-    assertShift(referenceBoundingBox, ownBoundingBox, Center, Top, QPointF(1.625, 22.875));
-    assertShift(referenceBoundingBox, ownBoundingBox, Center, Bottom, QPointF(1.625, 18.875));
-//    // TODO random test: top - bottom = height of box
-    assertShift(referenceBoundingBox, ownBoundingBox, Center, Left, QPointF(3.375, 20.875));
-    assertShift(referenceBoundingBox, ownBoundingBox, Center, Right, QPointF(-.125, 20.875));
-    assertShift(referenceBoundingBox, ownBoundingBox, Center, TopLeft, QPointF(3.375, 22.875));
-    assertShift(referenceBoundingBox, ownBoundingBox, Center, BottomLeft, QPointF(3.375, 18.875));
-    assertShift(referenceBoundingBox, ownBoundingBox, Center, TopRight, QPointF(-.125, 22.875));
-    assertShift(referenceBoundingBox, ownBoundingBox, Center, BottomRight, QPointF(-.125, 18.875));
-
-    assertShift(referenceBoundingBox, ownBoundingBox, Top, Center, QPointF(1.625, 14.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, Top, Top, QPointF(1.625, 16.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, Top, Bottom, QPointF(1.625, 12.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, Top, Left, QPointF(3.375, 14.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, Top, Right, QPointF(-.125, 14.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, Top, TopLeft, QPointF(3.375, 16.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, Top, BottomLeft, QPointF(3.375, 12.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, Top, TopRight, QPointF(-.125, 16.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, Top, BottomRight, QPointF(-.125, 12.375));
-
-    assertShift(referenceBoundingBox, ownBoundingBox, Bottom, Center, QPointF(1.625, 27.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, Bottom, Top, QPointF(1.625, 29.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, Bottom, Bottom, QPointF(1.625, 25.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, Bottom, Left, QPointF(3.375, 27.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, Bottom, Right, QPointF(-.125, 27.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, Bottom, TopLeft, QPointF(3.375, 29.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, Bottom, BottomLeft, QPointF(3.375, 25.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, Bottom, TopRight, QPointF(-.125, 29.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, Bottom, BottomRight, QPointF(-.125, 25.375));
-
-    assertShift(referenceBoundingBox, ownBoundingBox, Left, Center, QPointF(-8.625, 20.875));
-    assertShift(referenceBoundingBox, ownBoundingBox, Left, Top, QPointF(-8.625, 22.875));
-    assertShift(referenceBoundingBox, ownBoundingBox, Left, Bottom, QPointF(-8.625, 18.875));
-    assertShift(referenceBoundingBox, ownBoundingBox, Left, Left, QPointF(-6.875, 20.875));
-    assertShift(referenceBoundingBox, ownBoundingBox, Left, Right, QPointF(-10.375, 20.875));
-    assertShift(referenceBoundingBox, ownBoundingBox, Left, TopLeft, QPointF(-6.875, 22.875));
-    assertShift(referenceBoundingBox, ownBoundingBox, Left, BottomLeft, QPointF(-6.875, 18.875));
-    assertShift(referenceBoundingBox, ownBoundingBox, Left, TopRight, QPointF(-10.375, 22.875));
-    assertShift(referenceBoundingBox, ownBoundingBox, Left, BottomRight, QPointF(-10.375, 18.875));
-
-    assertShift(referenceBoundingBox, ownBoundingBox, Right, Center, QPointF(11.875, 20.875));
-    assertShift(referenceBoundingBox, ownBoundingBox, Right, Top, QPointF(11.875, 22.875));
-    assertShift(referenceBoundingBox, ownBoundingBox, Right, Bottom, QPointF(11.875, 18.875));
-    assertShift(referenceBoundingBox, ownBoundingBox, Right, Left, QPointF(13.625, 20.875));
-    assertShift(referenceBoundingBox, ownBoundingBox, Right, Right, QPointF(10.125, 20.875));
-    assertShift(referenceBoundingBox, ownBoundingBox, Right, TopLeft, QPointF(13.625, 22.875));
-    assertShift(referenceBoundingBox, ownBoundingBox, Right, BottomLeft, QPointF(13.625, 18.875));
-    assertShift(referenceBoundingBox, ownBoundingBox, Right, TopRight, QPointF(10.125, 22.875));
-    assertShift(referenceBoundingBox, ownBoundingBox, Right, BottomRight, QPointF(10.125, 18.875));
-
-    assertShift(referenceBoundingBox, ownBoundingBox, TopLeft, Center, QPointF(-8.625, 14.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, TopLeft, Top, QPointF(-8.625, 16.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, TopLeft, Bottom, QPointF(-8.625, 12.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, TopLeft, Left, QPointF(-6.875, 14.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, TopLeft, Right, QPointF(-10.375, 14.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, TopLeft, TopLeft, QPointF(-6.875, 16.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, TopLeft, BottomLeft, QPointF(-6.875, 12.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, TopLeft, TopRight, QPointF(-10.375, 16.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, TopLeft, BottomRight, QPointF(-10.375, 12.375));
-
-    assertShift(referenceBoundingBox, ownBoundingBox, BottomLeft, Center, QPointF(-8.625, 27.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, BottomLeft, Top, QPointF(-8.625, 29.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, BottomLeft, Bottom, QPointF(-8.625, 25.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, BottomLeft, Left, QPointF(-6.875, 27.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, BottomLeft, Right, QPointF(-10.375, 27.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, BottomLeft, TopLeft, QPointF(-6.875, 29.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, BottomLeft, BottomLeft, QPointF(-6.875, 25.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, BottomLeft, TopRight, QPointF(-10.375, 29.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, BottomLeft, BottomRight, QPointF(-10.375, 25.375));
-
-    assertShift(referenceBoundingBox, ownBoundingBox, TopRight, Center, QPointF(11.875, 14.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, TopRight, Top, QPointF(11.875, 16.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, TopRight, Bottom, QPointF(11.875, 12.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, TopRight, Left, QPointF(13.625, 14.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, TopRight, Right, QPointF(10.125, 14.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, TopRight, TopLeft, QPointF(13.625, 16.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, TopRight, BottomLeft, QPointF(13.625, 12.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, TopRight, TopRight, QPointF(10.125, 16.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, TopRight, BottomRight, QPointF(10.125, 12.375));
-
-    assertShift(referenceBoundingBox, ownBoundingBox, BottomRight, Center, QPointF(11.875, 27.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, BottomRight, Top, QPointF(11.875, 29.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, BottomRight, Bottom, QPointF(11.875, 25.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, BottomRight, Left, QPointF(13.625, 27.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, BottomRight, Right, QPointF(10.125, 27.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, BottomRight, TopLeft, QPointF(13.625, 29.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, BottomRight, BottomLeft, QPointF(13.625, 25.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, BottomRight, TopRight, QPointF(10.125, 29.375));
-    assertShift(referenceBoundingBox, ownBoundingBox, BottomRight, BottomRight, QPointF(10.125, 25.375));
+    for (auto testCase : testCases())
+      assertShift(referenceBoundingBox, ownBoundingBox, std::get<0>(testCase), std::get<1>(testCase), std::get<2>(testCase));
   }
+
+  void testMovingWithOffset() {
+    QRectF referenceBoundingBox(3.5, 5.125, 20.5, 13),
+        ownBoundingBox(10.375, -11.25, 3.5, 4);
+    QPointF offset(51.375, -10.375);
+    for (auto testCase : testCases())
+      assertShiftWithOffset(referenceBoundingBox, ownBoundingBox, std::get<0>(testCase), std::get<1>(testCase), offset, offset + std::get<2>(testCase));
+  }
+    // TODO random test: top - bottom = height of box
 };
