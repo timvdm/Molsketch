6d5bf7e57c83ce2c4cdf0e99f02e1f409066661e
Hendrik Vennekate <Hendrik.Vennekate@posteo.de>
2017-09-03 12:43:22 +0200

    reformat radical electron

--- libmolsketch/radicalelectron.cpp
+++ libmolsketch/radicalelectron.cpp
@@ -23,93 +23,93 @@
 #include <QPainter>
 namespace Molsketch {
 
-struct RadicalElectronPrivate {
-  qreal diameter;
-  QColor color;
-  BoundingBoxLinker linker;
-  bool operator==(const RadicalElectronPrivate& other) {
-    return diameter == other.diameter
-        && color == other.color
-        && linker == other.linker;
+  struct RadicalElectronPrivate {
+    qreal diameter;
+    QColor color;
+    BoundingBoxLinker linker;
+    bool operator==(const RadicalElectronPrivate& other) {
+      return diameter == other.diameter
+          && color == other.color
+          && linker == other.linker;
+    }
+  };
+
+
+  RadicalElectron::RadicalElectron(qreal diameter, BoundingBoxLinker linker, const QColor &color)
+    : d_ptr(new RadicalElectronPrivate)
+  {
+    Q_D(RadicalElectron);
+    d->diameter = diameter;
+
+    d->color = color;
+    d->linker = linker;
   }
-};
 
+  RadicalElectron::RadicalElectron(const RadicalElectron &other)
+    : QGraphicsItem(nullptr),
+      d_ptr(new RadicalElectronPrivate(*(other.d_ptr))) {}
 
-RadicalElectron::RadicalElectron(qreal diameter, BoundingBoxLinker linker, const QColor &color)
-  : d_ptr(new RadicalElectronPrivate)
-{
-  Q_D(RadicalElectron);
-  d->diameter = diameter;
+  RadicalElectron::~RadicalElectron() {}
 
-  d->color = color;
-  d->linker = linker;
-}
+  QRectF RadicalElectron::boundingRect() const {
+    if (!parentItem()) return QRectF();
+    Q_D(const RadicalElectron);
+    QRectF bounds(0,0,d->diameter,d->diameter);
+    bounds.translate(d->linker.getShift(parentItem()->boundingRect(), bounds));
+    return bounds;
+  }
 
-RadicalElectron::RadicalElectron(const RadicalElectron &other)
-  : QGraphicsItem(nullptr),
-    d_ptr(new RadicalElectronPrivate(*(other.d_ptr))) {}
+  void RadicalElectron::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) {
+    Q_UNUSED(option)
+    Q_UNUSED(widget)
+    Q_D(const RadicalElectron);
+    if (!parentItem()) return;
+    painter->drawEllipse(boundingRect());
+  }
 
-RadicalElectron::~RadicalElectron() {}
+  BoundingBoxLinker RadicalElectron::linker() const {
+    Q_D(const RadicalElectron);
+    return d->linker;
+  }
 
-QRectF RadicalElectron::boundingRect() const {
-  if (!parentItem()) return QRectF();
-  Q_D(const RadicalElectron);
-  QRectF bounds(0,0,d->diameter,d->diameter);
-  bounds.translate(d->linker.getShift(parentItem()->boundingRect(), bounds));
-  return bounds;
-}
+  bool RadicalElectron::operator==(const RadicalElectron &other) {
+    return *(other.d_ptr) == *(d_ptr);
+  }
 
-void RadicalElectron::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) {
-  Q_UNUSED(option)
-  Q_UNUSED(widget)
-  Q_D(const RadicalElectron);
-  if (!parentItem()) return;
-  painter->drawEllipse(boundingRect());
-}
+  QString RadicalElectron::xmlName() const {
+    return "radicalElectron";
+  }
 
-BoundingBoxLinker RadicalElectron::linker() const {
-  Q_D(const RadicalElectron);
-  return d->linker;
-}
+  void RadicalElectron::readAttributes(const QXmlStreamAttributes &attributes){
+    Q_D(RadicalElectron);
+    d->color = graphicsItem::extractColor(attributes);
+    d->diameter = attributes.value("diameter").toDouble();
+  }
 
-bool RadicalElectron::operator==(const RadicalElectron &other) {
-  return *(other.d_ptr) == *(d_ptr);
-}
+  QDebug operator<<(QDebug debug, const RadicalElectron &radicalElectron) {
+    debug.nospace() << "RadicalElectron(diameter: " << radicalElectron.d_func()->diameter
+                    << ", color: " << radicalElectron.d_func()->color
+                    << ", linker: " << radicalElectron.d_func()->linker
+                    << ")";
+    return debug;
+  }
 
-QString RadicalElectron::xmlName() const {
-  return "radicalElectron";
-}
+  QXmlStreamAttributes RadicalElectron::xmlAttributes() const {
+    Q_D(const RadicalElectron);
+    QXmlStreamAttributes attributes;
+    attributes.append("diameter", QString::number(d->diameter));
+    graphicsItem::addColor(attributes, d->color);
+    return attributes;
+  }
 
-void RadicalElectron::readAttributes(const QXmlStreamAttributes &attributes){
-  Q_D(RadicalElectron);
-  d->color = graphicsItem::extractColor(attributes);
-  d->diameter = attributes.value("diameter").toDouble();
-}
+  QList<const XmlObjectInterface *> RadicalElectron::children() const {
+    Q_D(const RadicalElectron);
+    return QList<const XmlObjectInterface*>() << &(d->linker);
+  }
 
-QDebug operator<<(QDebug debug, const RadicalElectron &radicalElectron) {
-  debug.nospace() << "RadicalElectron(diameter: " << radicalElectron.d_func()->diameter
-                  << ", color: " << radicalElectron.d_func()->color
-                  << ", linker: " << radicalElectron.d_func()->linker
-                  << ")";
-  return debug;
-}
-
-QXmlStreamAttributes RadicalElectron::xmlAttributes() const {
-  Q_D(const RadicalElectron);
-  QXmlStreamAttributes attributes;
-  attributes.append("diameter", QString::number(d->diameter));
-  graphicsItem::addColor(attributes, d->color);
-  return attributes;
-}
-
-QList<const XmlObjectInterface *> RadicalElectron::children() const {
-  Q_D(const RadicalElectron);
-  return QList<const XmlObjectInterface*>() << &(d->linker);
-}
-
-XmlObjectInterface *RadicalElectron::produceChild(const QString &name, const QString &type) {
-  Q_D(RadicalElectron);
-  if (d->linker.xmlName() == name) return &(d->linker);
-  return abstractXmlObject::produceChild(name, type);
-}
+  XmlObjectInterface *RadicalElectron::produceChild(const QString &name, const QString &type) {
+    Q_D(RadicalElectron);
+    if (d->linker.xmlName() == name) return &(d->linker);
+    return abstractXmlObject::produceChild(name, type);
+  }
 } // namespace Molsketch
