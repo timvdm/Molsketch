5520d0c9fd738cd707d3cb203a9e9092ed27e652
Hendrik Vennekate <Hendrik.Vennekate@posteo.de>
2017-11-21 18:40:43 +0100

    Add alignment action icons

--- libmolsketch/actions/alignmentaction.cpp
+++ libmolsketch/actions/alignmentaction.cpp
@@ -37,48 +37,49 @@ namespace Molsketch {
     return result;
   }
 
-#define ALIGNMENT_ACTION(DESCRIPTION, START, SHIFT, ACCUMULATOR) \
+#define ALIGNMENT_ACTION(DESCRIPTION, START, ICON, SHIFT, ACCUMULATOR) \
   class AlignmentActionImpl : public AlignmentAction { \
   Accumulator getAccumulator(int count) const override { Q_UNUSED(count) \
   return [=](const qreal& current, const graphicsItem* item) -> qreal { return ACCUMULATOR; } ; } \
   QPointF getShift(const graphicsItem* item, const qreal& targetValue) const override {return SHIFT; } \
   qreal initialValue() const override { return START; } \
-  public: AlignmentActionImpl(MolScene *scene) : AlignmentAction(DESCRIPTION, scene) {} \
+  public: AlignmentActionImpl(MolScene *scene) : AlignmentAction(DESCRIPTION, scene) \
+  { setIcon(QIcon(":images/" ICON ".svg")); } \
 }; \
   return new AlignmentActionImpl(scene);
 
   AlignmentAction *AlignmentAction::flushLeft(MolScene *scene) {
-    ALIGNMENT_ACTION("Align left", qInf(),
+    ALIGNMENT_ACTION("Align left", qInf(), "align-left",
                      QPointF(targetValue - item->boundingRect().left(), 0),
                      qMin(current, item->boundingRect().left()))
   }
 
   AlignmentAction *AlignmentAction::flushRight(MolScene *scene) {
-    ALIGNMENT_ACTION("Align right", -qInf(),
+    ALIGNMENT_ACTION("Align right", -qInf(), "align-right",
                      QPointF(targetValue - item->boundingRect().right(), 0),
                      qMax(current, item->boundingRect().right()))
   }
 
   AlignmentAction *AlignmentAction::atTop(MolScene *scene) {
-    ALIGNMENT_ACTION("Align top", qInf(),
+    ALIGNMENT_ACTION("Align top", qInf(), "align-top",
                      QPointF(0, targetValue - item->boundingRect().top()),
                      qMin(current, item->boundingRect().top()))
   }
 
   AlignmentAction *AlignmentAction::atBottom(MolScene *scene) {
-    ALIGNMENT_ACTION("Align bottom", -qInf(),
+    ALIGNMENT_ACTION("Align bottom", -qInf(), "align-bottom",
                      QPointF(0, targetValue - item->boundingRect().bottom()),
                      qMax(current, item->boundingRect().bottom()))
   }
 
   AlignmentAction *AlignmentAction::atVerticalCenter(MolScene *scene) {
-    ALIGNMENT_ACTION("Align vertically", 0,
+    ALIGNMENT_ACTION("Align vertically", 0, "align-v-center",
                      QPointF(0, targetValue - item->boundingRect().center().y()),
                      current + item->boundingRect().center().y() / count)
   }
 
   AlignmentAction *AlignmentAction::atHorizontalCenter(MolScene *scene) {
-    ALIGNMENT_ACTION("Align horizontally", 0,
+    ALIGNMENT_ACTION("Align horizontally", 0, "align-h-center",
                      QPointF(targetValue - item->boundingRect().center().x(), 0),
                      current + item->boundingRect().center().x() / count)
   }
