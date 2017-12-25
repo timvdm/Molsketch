ee6e18f6e1231667c1af802969f4d9ac9332ddc5
Hendrik Vennekate <Hendrik.Vennekate@posteo.de>
2017-12-25 17:44:41 +0100

    Prepare tests for refactoring of electron systems painting

--- instancecounters.cpp
+++ instancecounters.cpp
@@ -5,9 +5,11 @@ namespace Molsketch {
   class Molecule;
   class MoleculeModelItem;
   class LibraryModel;
+  class PiElectrons;
 }
 template<> int ForTesting<Molsketch::Molecule>::instanceCounter = 0;
 template<> int ForTesting<Molsketch::MoleculeModelItem>::instanceCounter = 0;
 template<> int ForTesting<Molsketch::LibraryModel>::instanceCounter = 0;
 template<> int ForTesting<QGraphicsItem>::instanceCounter = 0;
 template<> int ForTesting<Molsketch::RadicalElectron>::instanceCounter = 0;
+template<> int ForTesting<Molsketch::PiElectrons>::instanceCounter = 0;
