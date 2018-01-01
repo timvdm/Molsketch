5270d6ee39b6a9cd0b0ad1b2d546a2c4cb163526
Hendrik Vennekate <Hendrik.Vennekate@posteo.de>
2018-01-01 21:24:48 +0100

    Add test for undo command for settings item

---
+++ settingsitemundocommandunittest.h
@@ -0,0 +1,92 @@
+/***************************************************************************
+ *   Copyright (C) 2017 by Hendrik Vennekate                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <cxxtest/TestSuite.h>
+#include <settingsitem.h>
+#include <settingsfacade.h>
+#include <molscene.h>
+#include "utilities.h"
+
+using namespace Molsketch;
+
+const QString KEY("TestKey");
+const QString ALTERNATIVE_KEY("OtherTestKey");
+const qreal ORIGINAL_VALUE = 5.5;
+const qreal NEW_VALUE = 10.25;
+const QString DESCRIPTION("Change setting");
+
+class SettingsItemCommandUnitTest : public CxxTest::TestSuite {
+  DoubleSettingsItem *testItem, *secondTestItem;
+  SettingsFacade *facade;
+  MolScene *scene;
+public:
+  void setUp() {
+    scene = new MolScene;
+    facade = SettingsFacade::transientSettings(scene);
+    testItem = new DoubleSettingsItem(KEY, facade,scene);
+    secondTestItem = new DoubleSettingsItem(KEY, facade,scene);
+
+    testItem->set(ORIGINAL_VALUE);
+  }
+
+  void tearDown() {
+    delete scene;
+  }
+
+  void testRedoing() {
+    undoCommandForSettingsItem(testItem, NEW_VALUE, DESCRIPTION, scene)->execute();
+    TS_ASSERT_EQUALS(testItem->get(), NEW_VALUE);
+    TS_ASSERT_EQUALS(scene->stack()->index(), 1);
+    QS_ASSERT_EQUALS(scene->stack()->undoText(), DESCRIPTION);
+  }
+
+  void testUndoing() {
+    undoCommandForSettingsItem(testItem, NEW_VALUE, DESCRIPTION, scene)->execute();
+    scene->stack()->undo();
+    TS_ASSERT_EQUALS(testItem->get(), ORIGINAL_VALUE);
+    QS_ASSERT_EQUALS(scene->stack()->redoText(), DESCRIPTION);
+  }
+
+  void testMerging() {
+    undoCommandForSettingsItem(testItem, NEW_VALUE, DESCRIPTION, scene)->execute();
+    undoCommandForSettingsItem(testItem, ORIGINAL_VALUE, DESCRIPTION, scene)->execute();
+
+    TS_ASSERT_EQUALS(scene->stack()->index(), 1);
+    QS_ASSERT_EQUALS(scene->stack()->undoText(), DESCRIPTION);
+    TS_ASSERT_EQUALS(testItem->get(), ORIGINAL_VALUE);
+
+    scene->stack()->undo();
+    TS_ASSERT_EQUALS(scene->stack()->index(), 0);
+    TS_ASSERT_EQUALS(testItem->get(), ORIGINAL_VALUE);
+  }
+
+  void testNotMerging() {
+    undoCommandForSettingsItem(testItem, NEW_VALUE, DESCRIPTION, scene)->execute();
+    undoCommandForSettingsItem(secondTestItem, NEW_VALUE, DESCRIPTION, scene)->execute();
+
+    TS_ASSERT_EQUALS(scene->stack()->index(), 2);
+    TS_ASSERT_EQUALS(testItem->get(), NEW_VALUE);
+    TS_ASSERT_EQUALS(secondTestItem->get(), NEW_VALUE);
+  }
+
+  void testRedoingWithoutScene() {
+    undoCommandForSettingsItem(testItem, NEW_VALUE)->execute();
+    TS_ASSERT_EQUALS(testItem->get(), NEW_VALUE);
+  }
+};
