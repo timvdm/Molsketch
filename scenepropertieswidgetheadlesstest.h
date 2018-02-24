6c85925eaa9c7c233c57539b088eff894d2d5e30
Hendrik Vennekate (@blackMamba) <HVennekate@gmx.de>
2018-02-24 11:25:07 +0100

    Add test to ensure all settingsItems are connected (signal/slot)

--- scenepropertieswidgetheadlesstest.h
+++ scenepropertieswidgetheadlesstest.h
@@ -22,6 +22,7 @@
 #include <molscene.h>
 #include <scenepropertieswidget.h>
 #include <settingsitem.h>
+#include <settingsfacade.h>
 #include "utilities.h"
 
 using namespace Molsketch;
@@ -29,15 +30,32 @@ using namespace Molsketch;
 const qreal BOND_LINE_WIDTH = 5.5;
 const qreal ALTERNATE_LINE_WIDTH = 11.75;
 
+class SceneSettingsForTesting : public SceneSettings {
+public:
+  QList<const SettingsItem*> getAllSettingsItems() {
+    QList<const SettingsItem*> result;
+    for (auto child : children())
+      if (auto settingsItem = dynamic_cast<const SettingsItem*>(child))
+        result << settingsItem;
+    return result;
+  }
+  SceneSettingsForTesting() : SceneSettings(SettingsFacade::transientSettings()) {}
+  int connections(const char* signal) {
+    return receivers(signal);
+  }
+};
+
 class ScenePropertiesWidgetHeadlessTest : public CxxTest::TestSuite {
   MolScene *scene;
   ScenePropertiesWidget *propertiesWidget;
   QDoubleSpinBox *bondLineWidthControl;
   DoubleSettingsItem *bondLineWidthSetting;
+  SceneSettingsForTesting *settings;
 
 public:
   void setUp() override {
-    scene = new MolScene;
+    settings = new SceneSettingsForTesting;
+    scene = new MolScene(settings);
     propertiesWidget = scene->producePropertiesWidget();
     bondLineWidthControl = assertNotNull<QDoubleSpinBox>(propertiesWidget->findChild<QDoubleSpinBox*>("bondLineWidth"));
     bondLineWidthSetting = assertNotNull<DoubleSettingsItem>(scene->settings()->bondWidth());
@@ -92,5 +110,21 @@ public:
     TS_ASSERT_EQUALS(bondLineWidthControl->value(), BOND_LINE_WIDTH);
     TS_ASSERT_EQUALS(scene->stack()->index(), 0);
     TS_ASSERT_EQUALS(scene->stack()->count(), 1);
+  }
+
+  void testAllSettingsAreConnectedToUi() {
+    for (auto settingsItem : settings->getAllSettingsItems()) {
+      for (int i = settingsItem->metaObject()->methodOffset() ; i < settingsItem->metaObject()->methodCount(); ++i) {
+       auto method = settingsItem->metaObject()->method(i);
+       if (QMetaMethod::Signal == method.methodType())
+         TSM_ASSERT(
+               ("Expecting signal " + method.methodSignature() + " of " + settingsItem->xmlName() + " to be connected").toStdString().c_str(),
+               QObject::disconnect(settingsItem, method, 0, QMetaMethod()));
+       if (QMetaMethod::Slot == method.methodType())
+         TSM_ASSERT(
+               ("Expecting slot " + method.methodSignature() + " of " + settingsItem->xmlName() + " to be connected").toStdString().c_str(),
+               QObject::disconnect(0, QMetaMethod(), settingsItem, method));
+      }
+    }
   }
 };
