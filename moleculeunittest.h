06ae1d42969019b3101e41a25893b8e5363967da
Hendrik Vennekate <Hendrik.Vennekate@posteo.de>
2018-03-18 22:20:19 +0100

    Add more tests for electron system algorithms

--- moleculeunittest.h
+++ moleculeunittest.h
@@ -30,20 +30,35 @@ CLASS_FOR_TESTING(PiElectrons)
 
 class MoleculeUnitTest : public CxxTest::TestSuite {
   Molecule *molecule;
-  Atom *atomA, *atomB;
+  Atom *atomA, *atomB, *atomC, *atomD, *atomE, *atomF;
 
   void setUpAtoms() {
     atomA = new Atom;
     atomB = new Atom;
+    atomC = new Atom;
+    atomD = new Atom;
+    atomE = new Atom;
+    atomF = new Atom;
     molecule->addAtom(atomA);
     molecule->addAtom(atomB);
+    molecule->addAtom(atomC);
+    molecule->addAtom(atomD);
+    molecule->addAtom(atomE);
+    molecule->addAtom(atomF);
   }
 
   void performAssertionsOnPiElectronSystem(const ElectronSystem* electronSystem) {
-    TS_ASSERT_EQUALS(electronSystem->numAtoms(), 2);
-    TS_ASSERT(electronSystem->atoms().contains(atomA));
-    TS_ASSERT(electronSystem->atoms().contains(atomB));
-    TS_ASSERT_EQUALS(electronSystem->numElectrons(), 2);
+    performAssertionsOnPiElectronSystem(electronSystem, atomA, atomB);
+  }
+
+  void performAssertionsOnPiElectronSystem(const ElectronSystem* electronSystem, Atom* first, Atom* second) {
+    performAssertionsOnPiElectronSystem(electronSystem, {first, second});
+  }
+
+  void performAssertionsOnPiElectronSystem(const ElectronSystem* electronSystem, QSet<Atom*> expectedAtoms, int additionalElectrons = 0) {
+    QS_ASSERT_EQUALS(electronSystem->atoms().toSet(), expectedAtoms);
+    TS_ASSERT_EQUALS(electronSystem->numAtoms(), expectedAtoms.size());
+    TS_ASSERT_EQUALS(electronSystem->numElectrons(), expectedAtoms.size() + additionalElectrons);
     TS_ASSERT_EQUALS(electronSystem->type(), ElectronSystem::PiType);
   }
 
@@ -75,7 +90,7 @@ public:
     setUpAtoms();
     molecule->addBond(atomA, atomB, Bond::Single);
     molecule->updateElectronSystems();
-    TS_ASSERT_EQUALS(molecule->m_electronSystems.size(), 0);
+    TS_ASSERT(molecule->m_electronSystems.empty());
   }
 
   void testOnePiElectronSystemForDoubleBond() {
@@ -95,6 +110,28 @@ public:
     TS_ASSERT_EQUALS(molecule->m_electronSystems.size(), 2);
     performAssertionsOnPiElectronSystem(molecule->m_electronSystems.first());
     performAssertionsOnPiElectronSystem(molecule->m_electronSystems.last());
+  }
+
+  void testTwoPiElectronSystemsForAlleneBonds() {
+    setUpAtoms();
+    molecule->addBond(atomB, atomA, Bond::DoubleSymmetric);
+    molecule->addBond(atomB, atomC, Bond::DoubleSymmetric);
+    molecule->updateElectronSystems();
+    // TODO array/container/iterable/iterator assertion
+    TS_ASSERT_EQUALS(molecule->m_electronSystems.size(), 2);
+    performAssertionsOnPiElectronSystem(molecule->m_electronSystems.first());
+    performAssertionsOnPiElectronSystem(molecule->m_electronSystems.last(), {atomB, atomC});
+  }
+
+  void testTwoPiElectronSystemsForAlleneBondsReverse() {
+    setUpAtoms();
+    molecule->addBond(atomB, atomA, Bond::DoubleSymmetric);
+    molecule->addBond(atomC, atomB, Bond::DoubleSymmetric);
+    molecule->updateElectronSystems();
+    // TODO array/container/iterable/iterator assertion
+    TS_ASSERT_EQUALS(molecule->m_electronSystems.size(), 2);
+    performAssertionsOnPiElectronSystem(molecule->m_electronSystems.first());
+    performAssertionsOnPiElectronSystem(molecule->m_electronSystems.last(), {atomB, atomC});
   }
 
   void testPiElectronSystemForAtomWithSingleUnboundElectron() {
@@ -129,5 +166,125 @@ public:
     TS_ASSERT_EQUALS(molecule->m_electronSystems.size(), 2);
     performAssertionsOnAtomicElectronSystem(molecule->m_electronSystems.first(), 2);
     performAssertionsOnAtomicElectronSystem(molecule->m_electronSystems.last(), 2);
+  }
+
+  void testPiElectronSystemsNotMergedIfShareAtom() {
+    setUpAtoms();
+    molecule->addBond(atomA, atomB, Bond::DoubleSymmetric);
+    molecule->addBond(atomC, atomB, Bond::DoubleSymmetric);
+    molecule->updateElectronSystems();
+    TS_ASSERT_EQUALS(molecule->m_electronSystems.size(), 2);
+    performAssertionsOnPiElectronSystem(molecule->m_electronSystems.first());
+    performAssertionsOnPiElectronSystem(molecule->m_electronSystems.last(), atomC, atomB);
+  }
+
+  void testPiElectronSystemsMergedIfNeighboringAtoms() {
+    setUpAtoms();
+    molecule->addBond(atomA, atomB, Bond::DoubleSymmetric);
+    molecule->addBond(atomB, atomC, Bond::Single);
+    molecule->addBond(atomC, atomD, Bond::DoubleSymmetric);
+    molecule->updateElectronSystems();
+    TS_ASSERT_EQUALS(molecule->m_electronSystems.size(), 1);
+    performAssertionsOnPiElectronSystem(molecule->m_electronSystems.first(), {atomA, atomB, atomC, atomD});
+  }
+
+  void testPiElectronSystemsNotMergedIfAtomPartOfPiBond() {
+    setUpAtoms();
+    molecule->addBond(atomA, atomB, Bond::DoubleSymmetric);
+    atomA->setNumUnpairedElectrons(1);
+    molecule->updateElectronSystems();
+    TS_ASSERT_EQUALS(molecule->m_electronSystems.size(), 2);
+    performAssertionsOnPiElectronSystem(molecule->m_electronSystems.first());
+    performAssertionsOnAtomicElectronSystem(molecule->m_electronSystems.last(), 1);
+  }
+
+  void testPiElectronSystemMergedForAtomAndNeighboringBond() {
+    setUpAtoms();
+    molecule->addBond(atomA, atomB, Bond::DoubleSymmetric);
+    molecule->addBond(atomB, atomC, Bond::Single);
+    atomC->setNumUnpairedElectrons(1);
+    molecule->updateElectronSystems();
+    TS_ASSERT_EQUALS(molecule->m_electronSystems.size(), 1);
+    performAssertionsOnPiElectronSystem(molecule->m_electronSystems.first(), {atomA, atomB, atomC});
+  }
+
+  void testThreePiElectronsOnAtomOnlyTwoGetMerged() {
+    setUpAtoms();
+    molecule->addBond(atomA, atomB, Bond::Single);
+    molecule->addBond(atomB, atomC, Bond::DoubleSymmetric);
+    atomA->setNumUnpairedElectrons(3);
+    molecule->updateElectronSystems();
+    TS_ASSERT_EQUALS(molecule->m_electronSystems.size(), 2);
+    performAssertionsOnPiElectronSystem(molecule->m_electronSystems.first(), {atomA, atomB, atomC}, 1);
+    performAssertionsOnAtomicElectronSystem(molecule->m_electronSystems.last(), 1);
+  }
+
+  void testBenzene() {
+    setUpAtoms();
+    molecule->addBond(atomA, atomB, Bond::DoubleSymmetric);
+    molecule->addBond(atomB, atomC, Bond::Single);
+    molecule->addBond(atomC, atomD, Bond::DoubleSymmetric);
+    molecule->addBond(atomD, atomE, Bond::Single);
+    molecule->addBond(atomE, atomF, Bond::DoubleSymmetric);
+    molecule->addBond(atomF, atomA, Bond::Single);
+    molecule->updateElectronSystems();
+    TS_ASSERT_EQUALS(molecule->m_electronSystems.size(), 1);
+    performAssertionsOnPiElectronSystem(molecule->m_electronSystems.first(), {atomA, atomB, atomC, atomD, atomE, atomF});
+  }
+
+  void testBenzyneA() {
+    setUpAtoms();
+    molecule->addBond(atomA, atomB, Bond::DoubleSymmetric);
+    molecule->addBond(atomB, atomC, Bond::Single);
+    molecule->addBond(atomC, atomD, Bond::Triple);
+    molecule->addBond(atomD, atomE, Bond::Single);
+    molecule->addBond(atomE, atomF, Bond::DoubleSymmetric);
+    molecule->addBond(atomF, atomA, Bond::Single);
+    molecule->updateElectronSystems();
+    TS_ASSERT_EQUALS(molecule->m_electronSystems.size(), 2);
+    performAssertionsOnPiElectronSystem(molecule->m_electronSystems.first(), {atomA, atomB, atomC, atomD, atomE, atomF});
+    performAssertionsOnPiElectronSystem(molecule->m_electronSystems.last(), {atomC, atomD});
+  }
+
+  void testBenzyneB() {
+    setUpAtoms();
+    molecule->addBond(atomA, atomB, Bond::DoubleSymmetric);
+    molecule->addBond(atomB, atomC, Bond::DoubleSymmetric);
+    molecule->addBond(atomC, atomD, Bond::DoubleSymmetric);
+    molecule->addBond(atomD, atomE, Bond::Single);
+    molecule->addBond(atomE, atomF, Bond::DoubleSymmetric);
+    molecule->addBond(atomF, atomA, Bond::Single);
+    molecule->updateElectronSystems();
+    TS_ASSERT_EQUALS(molecule->m_electronSystems.size(), 2);
+    performAssertionsOnPiElectronSystem(molecule->m_electronSystems.first(), {atomA, atomB, atomC, atomD, atomE, atomF});
+    performAssertionsOnPiElectronSystem(molecule->m_electronSystems.last(), {atomB, atomC});
+  }
+
+  void testPyridine() {
+    setUpAtoms();
+    molecule->addBond(atomA, atomB, Bond::DoubleSymmetric);
+    molecule->addBond(atomB, atomC, Bond::Single);
+    molecule->addBond(atomC, atomD, Bond::DoubleSymmetric);
+    molecule->addBond(atomD, atomE, Bond::Single);
+    molecule->addBond(atomE, atomF, Bond::DoubleSymmetric);
+    molecule->addBond(atomF, atomA, Bond::Single);
+    atomA->setNumUnpairedElectrons(2);
+    molecule->updateElectronSystems();
+    TS_ASSERT_EQUALS(molecule->m_electronSystems.size(), 2);
+    performAssertionsOnPiElectronSystem(molecule->m_electronSystems.last(), {atomA}, 1);
+    performAssertionsOnPiElectronSystem(molecule->m_electronSystems.first(), {atomA, atomB, atomC, atomD, atomE, atomF});
+  }
+
+  void testPyrrole() {
+    setUpAtoms();
+    molecule->addBond(atomA, atomB, Bond::DoubleSymmetric);
+    molecule->addBond(atomB, atomC, Bond::Single);
+    molecule->addBond(atomC, atomD, Bond::Single);
+    molecule->addBond(atomD, atomE, Bond::DoubleSymmetric);
+    molecule->addBond(atomE, atomA, Bond::Single);
+    atomC->setNumUnpairedElectrons(2);
+    molecule->updateElectronSystems();
+    TS_ASSERT_EQUALS(molecule->m_electronSystems.size(), 1);
+    performAssertionsOnPiElectronSystem(molecule->m_electronSystems.last(), {atomA, atomB, atomC, atomD, atomE}, 1);
   }
 };
