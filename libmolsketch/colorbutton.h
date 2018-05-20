3059df3f9f347de9f3376920573f67f57bbda0fb
Hendrik Vennekate (@blackMamba) <HVennekate@gmx.de>
2018-05-20 11:08:15 +0200

    Update copyright notices

--- libmolsketch/colorbutton.h
+++ libmolsketch/colorbutton.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2017 by Hendrik Vennekate                               *
+ *   Copyright (C) 2017 Hendrik Vennekate                                  *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -14,7 +14,7 @@
  *   You should have received a copy of the GNU General Public License     *
  *   along with this program; if not, write to the                         *
  *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
  ***************************************************************************/
 #ifndef COLORBUTTON_H
 #define COLORBUTTON_H
